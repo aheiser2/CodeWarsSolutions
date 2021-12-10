9b9537d5841bf52e646977381c042ed5ad27530e
Abby Heiser <85967450+aheiser2@users.noreply.github.com>
2021-12-09 19:02:44 -0600

    Create String-Repeat.js

---
+++ String-Repeat.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e 
+
+function repeatStr (n, s) {
+  var repeatedString = ""
+  for(let i = 1; i<=n; i++){
+    repeatedString += s
+  }
+  return repeatedString;
+}
