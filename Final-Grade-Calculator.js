cf5a3063e4f9becb70605acbbe7e3fac205f7f15
Abby Heiser <aheiser2@yahoo.com>
2021-12-16 23:01:47 -0600

    Final grade calculator

---
+++ Final-Grade-Calculator.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/5ad0d8356165e63c140014d4/
+
+
+function finalGrade (exam, projects) {
+let finalGrade = 0;
+    if (exam > 90 || projects > 10) {
+      finalGrade = 100;
+  } else if (exam > 75 && projects >= 5 ){
+      finalGrade = 90;
+  } else if ( exam > 50 && projects >= 2) {
+      finalGrade = 75;
+  } else {
+      finalGrade = 0;
+  }
+    return finalGrade;
+}
